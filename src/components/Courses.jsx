be34ca2a5cee19aa9846f5f151175d6dd09ad4b7
Sergey87 <serge.techno@gmail.com>
2023-07-11 23:24:26 +0300

    added course sorting - 1 part

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,7 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import courses from '../data/courses';
+import { useState } from 'react';
 
 const Courses = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const [sortKey, setSortKey] = useState(query.sort);
+  console.log(sortKey);
+
+  // console.log(location);
+  // console.log(query);
+
+  const [sortedCourses, setSortedCourses] = useState();
+
   return (
     <>
       <h1>Courses</h1>
